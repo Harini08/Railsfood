0cd65afb4f0677eef7121c011839ba16dec5ed46
Harini MV <harini.manivannan@gmail.com>
2013-06-22 15:27:16 +0100

    first app working version

---
+++ app/models/ingredient_use.rb
@@ -0,0 +1,5 @@
+class IngredientUse < ActiveRecord::Base
+  belongs_to :recipe
+  belongs_to :ingredient
+  attr_accessible :ingredient_id, :recipe_id
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsfood::Application.config.secret_token = '05d8009e215d30b807a853ab24d587438704903eef26651a9ccae16b306c3ab92359f63af2f6efa4e440bd407d65805c2a52c81c7fb14267cf5e5f512f6a7824'
